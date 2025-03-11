1f0030e829d9382c08bf087a6cd46a83cae5451a
Harshada2904 <nandeharshada2016@gmail.com>
2025-03-11 14:37:39 +0530

    Delete OOPJ/Qno17.java

--- OOPJ/Qno17.java
+++
@@ -1,7 +0,0 @@
-class Qno17{
-	public static void main(String args[]){
-		int a=5, b=65, c=2, d=31;
-		String s =(a<b && a< c && a<d)? a +" is samller": ((b < c && b< d )?b+ " is smaller" : (c<d ? c + " is smaller":d+" is smaller"));
-		System.out.println(s);
-	}
-}
