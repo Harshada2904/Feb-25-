702bb6e8e7d2cea89051501bf849f56f877a685b
Harshada2904 <nandeharshada2016@gmail.com>
2025-03-11 14:35:57 +0530

    Delete OOPJ/GreateatNum.java

--- OOPJ/GreateatNum.java
+++
@@ -1,7 +0,0 @@
-class Qno6{
-	public static void main(String args[]){
-		int a = 10, b=6, c=40;
-		String s =(a>b && a>c)? (a + " is greatest number "): (b>a && b>c)? (b + " is greatest number"):(c + " is greatest number");
-		System.out.println(s);
-	}
-}
