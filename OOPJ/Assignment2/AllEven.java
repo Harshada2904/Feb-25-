9428583a08585b72495ac6073fba237a27d1c25a
Harshada2904 <nandeharshada2016@gmail.com>
2025-03-11 15:08:14 +0530

    Add files via upload

---
+++ OOPJ/Assignment2/AllEven.java
@@ -0,0 +1,9 @@
+public class AllEven{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if ((i & 1) == 0) {
+                System.out.println(i );
+            }
+        }
+    }
+}
---
+++ OOPJ/Assignment2/Increment.java
@@ -0,0 +1,6 @@
+public class Increment{
+	static public void main(String me[]){
+		int a = 5;
+		System.out.println(-~a);
+	}
+}
