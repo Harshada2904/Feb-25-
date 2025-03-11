88acd5d2001d088971307ac44d7f2485b6a503a5
Harshada2904 <nandeharshada2016@gmail.com>
2025-03-11 14:36:11 +0530

    Delete OOPJ/Vowel1.java

--- OOPJ/Vowel1.java
+++
@@ -1,7 +0,0 @@
-class Vowel1{
-	public static void main(String args[]){
-		char ch = 'b';
-		String s =(ch == 'a'|| ch == 'e' ||ch =='i' ||ch =='o' || ch =='u' || ch =='A'||ch =='E' ||ch =='I' ||ch =='O' ||ch =='U')? (ch + " is Vowel"): (ch + " is consonant");
-		System.out.println(s);
-	}
-}
